9ac04b410fad5b204cf40e935dfd7f0b0109f3d1
Rudraraj30 <rathodrudraraj1212@gmail.com>
2025-05-14 23:37:30 +0530

    Backend basicapp commit

---
+++ FLIPTREND_BACKEND/src/main/java/com/example/fliptrend/FliptrendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fliptrend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FliptrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FliptrendApplication.class, args);
+	}
+
+}
---
+++ FLIPTREND_BACKEND/src/test/java/com/example/fliptrend/FliptrendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fliptrend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FliptrendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
